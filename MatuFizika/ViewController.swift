8c12305886aad173bde41a3080e5cfdc3335db37
leo <leutrim.konjuhi95@gmail.com>
2020-06-23 20:56:55 +0200

    Initial Commit

---
+++ MatuFizika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatuFizika
+//
+//  Created by leotrim konjuhi on 6/23/20.
+//  Copyright © 2020 LEOTRIM KONJUHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
